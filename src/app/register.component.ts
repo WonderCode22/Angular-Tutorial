00486c04a17c4c45f1d02b3926ef1c6d58e957a7
YukiMinami22 <phoenix22@tutanota.com>
2017-08-31 00:38:36 -0700

    MDF Validation with FormBuilder Class

--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -1,22 +1,39 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
     selector: 'register-component',
     templateUrl: 'register1.component.html'
 })
 
-export class RegisterComponent{
+export class RegisterComponent implements OnInit{
+  /*
     onSubmit(){
       console.log(this.userForm.value);
     }
     userForm = new FormGroup({
       name: new FormControl("KimJuYong",[Validators.required, Validators.minLength(4), Validators.maxLength(10)]),
-      email: new FormControl(),
+      email: new FormControl([Validators.pattern]),
       address: new FormGroup({
         street: new FormControl(),
         city: new FormControl()
       }),
       pswd: new FormControl()
     });
+  */
+  userForm: FormGroup;
 
+  constructor(private _formBuilder: FormBuilder){
+  }
+
+  ngOnInit(){
+    this.userForm = this._formBuilder.group({
+      name: ["KimJuYong", [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+      email: [],
+      address: this._formBuilder.group({
+        street: [],
+        city: []
+      }),
+      pswd: []
+    })
+  }
 }
